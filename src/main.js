791aea24a960b89429f77d77bcc0dc7ae4574096
Mate Steinforth <mate1@gmx.net>
2025-05-26 19:27:46 +0200

    display shader works

--- src/main.js
+++ src/main.js
@@ -1,24 +1,16 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as glsl from 'glsl-canvas-js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const canvas = document.getElementById('shader-canvas');
 
-setupCounter(document.querySelector('#counter'))
+fetch('/shader.frag')
+  .then(res => res.text())
+  .then(fragmentShader => {
+    const sandbox = new glsl.Canvas(canvas);
+    sandbox.load(fragmentShader);
+    function resize() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    }
+    window.addEventListener('resize', resize);
+    resize();
+  });
